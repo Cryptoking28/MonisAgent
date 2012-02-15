415e37440d6432c4d152086bb61c3c67bf9da3e3
Cryptoking28 <48272573+Cryptoking28@users.noreply.github.com>
2012-02-15 12:50:58 -0800

    Clean up event handler scoping

--- test/service.test.js
+++ test/service.test.js
@@ -9,7 +9,7 @@ var testLicense = 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b';
 exports['test conn'] = function(beforeExit, assert) {
     var nr = service.createMonisAgentService(agent, testLicense, 'staging-collector.monisagent.com', 80);
     var connected = false;
-    nr.on('connect', function(error) {
+    nr.on('connect', function() {
         connected = true;
     });
     nr.connect();
