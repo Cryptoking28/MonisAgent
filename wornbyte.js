f3c37004097c1fa74b3bac7f73e61b4f58285de7
mert <mert@helius.xyz>
2012-11-27 15:57:18 -0800

    Documented configuration variables.

--- wornbyte.js
+++ wornbyte.js
@@ -1,8 +1,22 @@
+/**
+ * Monis Agent agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
 exports.config = {
-  app_name           : ['My Application'],
-  license_key        : 'license key here',
-  log_level          : 'trace',
-  transaction_tracer : {
-    enabled : true
-  }
+  /**
+   * Array of application names.
+   */
+  app_name : ['My Application'],
+  /**
+   * Your Monis Agent license key.
+   */
+  license_key : 'license key here',
+  /**
+   * Level at which to log. 'trace' is most useful to Monis Agent when diagnosing
+   * issues with the agent, 'info' and higher will impose the least overhead on
+   * production applications.
+   */
+  log_level : 'debug'
 };
