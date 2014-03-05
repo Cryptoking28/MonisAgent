0b7adc40b8c1755a4ef9d4cc7b17d7ec80e35f20
jon wong <j@jnwng.com>
2014-03-05 11:21:23 -0800

    ssl: keep-alive for pipelined HTTP requests
    Minimizes TLS socket creation overhead by reusing TLS sockets across
    multiple requests.

--- test/integration/ssl-api-error-data.tap.js
+++ test/integration/ssl-api-error-data.tap.js
@@ -12,7 +12,7 @@ test("Collector API should send errors to staging-collector.monisagent.com", fun
         'app_name'    : 'node.js Tests',
         'license_key' : 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b',
         'host'        : 'staging-collector.monisagent.com',
-        'port'        : 80,
+        'port'        : 443,
         'ssl'         : true,
         'logging'     : {
           'level' : 'trace'
