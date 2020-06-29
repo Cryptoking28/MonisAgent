33674b3fda3014c4fc2e8b63a9c19403468713c3
mert <mert@helius.xyz>
2020-06-29 09:47:59 -0700

    Adds copyright header to JS files.

--- bin/ca-gen.js
+++ bin/ca-gen.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+
+/*
+ * Copyright 2020 Monis Agent Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 'use strict'
 /* eslint-disable no-console */
 
