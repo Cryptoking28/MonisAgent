452510c0af6b2bcf24445babcd1151bfbb4f2c8f
jon wong <j@jnwng.com>
2020-11-06 10:43:45 -0800

    Update example to not use deprecated methods

--- examples/api/distributed-tracing/example1-background.js
+++ examples/api/distributed-tracing/example1-background.js
@@ -3,23 +3,24 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-var monisagent = require('monisagent')
+const monisagent = require('monisagent')
+
 // Give the agent some time to start up.
 setTimeout(runTest, 2000)
 
 function runTest() {
   monisagent.startWebTransaction('Custom web transaction', function() {
     // Call monisagent.getTransaction to retrieve a handle on the current transaction.
-    var transactionHandle = monisagent.getTransaction()
+    let transactionHandle = monisagent.getTransaction()
 
     // Generate the payload right before creating the linked transaction.
-    var payload = transactionHandle.createDistributedTracePayload()
-    var jsonPayload = payload.text()
+    let headers = {}
+    transactionHandle.insertDistributedTraceHeaders(headers)
 
     monisagent.startBackgroundTransaction('Background task', function executeTransaction() {
-      var backgroundHandle = monisagent.getTransaction()
+      let backgroundHandle = monisagent.getTransaction()
       // Link the nested transaction by accepting the payload with the background transaction's handle
-      backgroundHandle.acceptDistributedTracePayload(jsonPayload)
+      backgroundHandle.acceptDistributedTraceHeaders(headers)
       // End the transactions
       backgroundHandle.end(transactionHandle.end)
     })
