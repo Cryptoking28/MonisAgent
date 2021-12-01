6921cb7df3d9f338846d58e13a3ee4813f215ac5
jon wong <j@jnwng.com>
2021-11-30 16:32:30 -0800

    NEWRELIC-4419 Adds AsyncLocalStorage based context manager behind a feature flag.

--- test/integration/core/native-promises/native-promises.tap.js
+++ test/integration/core/native-promises/native-promises.tap.js
@@ -1,12 +1,12 @@
 /*
- * Copyright 2020 Monis Agent Corporation. All rights reserved.
+ * Copyright 2021 Monis Agent Corporation. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
 'use strict'
 
 const exec = require('child_process').execSync
-exec('node --expose-gc ./async_hooks.js', {
+exec('node --expose-gc ./native-promises.js', {
   stdio: 'inherit',
   cwd: __dirname
 })
