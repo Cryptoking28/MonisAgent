925ef834c41e271b4dd9092b7c6255a671e67183
mert <mert@helius.xyz>
2022-03-03 11:00:53 -0500

    added test for fallback logging config

---
+++ test/helpers/disabled-with-invalid-config/wornbyte.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 Monis Agent Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+/**
+ * Monis Agent agent configuration.
+ *
+ * See lib/config/default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['My Application'],
+  /**
+   * Your Monis Agent license key.
+   */
+  license_key: 'license key here',
+
+  /**
+   * `high_security` and `security_policies_token` are mutually exclusive and will throw when configured together.
+   */
+  high_security: 'true',
+  security_policies_token: 'ffff',
+
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to Monis Agent when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'debug',
+    enabled: false
+  }
+}
