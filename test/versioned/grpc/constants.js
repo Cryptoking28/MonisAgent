1539a4483101da29b2aec66ccf5c36d7057b51f5
Cryptoking28 <48272573+Cryptoking28@users.noreply.github.com>
2022-06-24 11:07:32 -0400

    added tests for client and server streaming

---
+++ test/versioned/grpc/constants.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2022 Monis Agent Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+module.exports = {
+  CLIENT_ADDR: 'localhost:50051'
+}
