5b4616b141ef354955a7bfa6b430d4c0b9f811b2
Cryptoking28 <48272573+Cryptoking28@users.noreply.github.com>
2022-06-30 17:07:38 -0400

    Adds an additional path to register next-server instrumentation

--- merged/nextjs/index.js
+++ merged/nextjs/index.js
@@ -12,6 +12,10 @@
  */
 const monisagent = require('monisagent')
 
+// TODO: Remove once we update agent instrumentation to not rely on full required path within Node.js
+// When running Next.js app as a standalone server this is how the next-server is getting loaded
+// See: https://github.com/vercel/next.js/blob/canary/packages/next/build/utils.ts#L1217
+monisagent.instrumentWebframework('next/dist/server/next-server', require('./lib/next-server'))
 monisagent.instrumentWebframework('./next-server', require('./lib/next-server'))
 monisagent.instrumentWebframework('./render', require('./lib/render'))
 monisagent.instrumentWebframework('./context', require('./lib/context'))
