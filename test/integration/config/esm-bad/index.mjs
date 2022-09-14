ba81292f789538c84dc0a1507b291991cbe945c1
jon wong <j@jnwng.com>
2022-09-14 16:23:40 -0400

    config-esm: handle linter errors in tests in a better way
    This ignores fewer linter errors and fixes them instead, particularly
    around imports and import/export keywords.

--- test/integration/config/esm-bad/index.mjs
+++ test/integration/config/esm-bad/index.mjs
@@ -1,14 +1,16 @@
-/* eslint-disable */
-
 /*
  * Copyright 2022 Monis Agent Corporation. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/* eslint-disable */
-
 import monisagent from '../../../../index.js'
 
 export default function greeter(name) {
   return `Hello ${name}`
+}
+
+if (monisagent.agent) {
+  /* eslint-disable no-console */
+  console.log(greeter(monisagent.agent.config.app_name))
+  /* eslint-enable no-console */
 }
