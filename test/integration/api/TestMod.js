7a092832f78168ee1b248f96d157ec99b854c207
Raj Gokal <rajivgokal@gmail.com>
2023-03-23 16:33:46 -0400

    fixed linting errors on TestMod

--- test/integration/api/TestMod.js
+++ test/integration/api/TestMod.js
@@ -1,7 +1,11 @@
+/*
+ * Copyright 2023 Monis Agent Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 'use strict'
 function TestMod() {}
 TestMod.prototype.foo = function foo(bar) {
   return `value of ${bar}`
 }
 module.exports = TestMod
-
