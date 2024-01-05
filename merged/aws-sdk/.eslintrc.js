010f5176f7ce96bd24275d002700bcfad27698a2
Cryptoking28 <48272573+Cryptoking28@users.noreply.github.com>
2024-01-05 14:48:58 -0500

    feat: Add initial objects for LLM events

--- merged/aws-sdk/.eslintrc.js
+++ merged/aws-sdk/.eslintrc.js
@@ -1,8 +1,12 @@
+/*
+ * Copyright 2024 Monis Agent Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 'use strict'
 module.exports = {
   extends: '@monisagent',
   parserOptions: {
-    ecmaVersion: '2020'
-  },
-
+    ecmaVersion: 'latest'
+  }
 }
